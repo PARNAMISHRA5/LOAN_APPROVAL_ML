ec2ec873daa6531227cb1642c8a66ad3cff1e262
codebrewers <codebrewers4@gmail.com>
2024-04-29 08:52:01 +0530

    hey

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ model = pickle.load(open('loan_model.pkl', 'rb'))
 @app.route('/', methods=['GET', 'POST'])
 def home():
     prediction_text = ""
-    form_visible = True  # Controls visibility of the form
+    form_visible = True  
     if request.method == 'POST':
         try:
-            # Collect all form data
+            
             input_features = [float(x) for x in request.form.values()]
             features_value = [np.array(input_features)]
-            # Make prediction
+            
             prediction = model.predict(features_value)
             prediction_text = "Loan Approved" if prediction[0] == 1 else "Loan Not Approved"
-            form_visible = False  # Hide the form when prediction is made
+            form_visible = False  
         except Exception as e:
             prediction_text = str(e)
             form_visible = True
